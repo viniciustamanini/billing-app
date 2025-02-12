4c46ec2091684b57c815da9e22d6d8ad11a9990f
Vinicius <vinitamanini@hotmail.com>
2025-02-12 00:49:14 -0300

    fix users insert/update params

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,11 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :cpf)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :first_name, :last_name, :cpf)
+  end
+end
