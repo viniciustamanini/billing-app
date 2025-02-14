b7d749995c03ead848c957ebc6f30aadfaaf715b
Vinicius <vinitamanini@hotmail.com>
2025-02-13 22:38:54 -0300

    add locale files and index page layout

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module BillingApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.available_locales = [ :en, :pt ]
+    config.i18n.default_locale = :en
   end
 end
