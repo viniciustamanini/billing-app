2cfc38b1ee6fb655e854d4f2304a9825e63ca95b
Vinicius <vinitamanini@hotmail.com>
2025-02-16 12:13:58 -0300

    auth layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  layout :layout_by_resource
 
   before_action :set_locale
 
@@ -10,5 +11,15 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     { locale: I18n.locale }
+  end
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "auth"
+    else
+      "application"
+    end
   end
 end
