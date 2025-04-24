d4b8c969c9073ebea3d7259a6bc268b3192b88c7
Vinicius <vinitamanini@hotmail.com>
2025-04-24 00:03:57 -0300

    delete old dashboard controller

--- app/controllers/dashboard_controller.rb
+++
@@ -1,8 +0,0 @@
-class DashboardController < ApplicationController
-  before_action :authenticate_user!
-
-  def index
-    @users = User.limit(5)
-    @profiles = current_user.profiles.includes(:company, :profile_type)
-  end
-end
