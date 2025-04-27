209d64a77d2c114ba5fda0688ef17ce82f1a924c
Vinicius Tamanini <vinitamanini@hotmail.com>
2025-04-27 08:06:59 -0400

    fix migrations

--- db/migrate/20250212015956_create_invoices.rb
+++ db/migrate/20250212015956_create_invoices.rb
@@ -1,7 +1,6 @@
 class CreateInvoices < ActiveRecord::Migration[8.0]
   def change
     create_table :invoices do |t|
-      t.references :customer, null: false, foreign_key: true
       t.date :issued_date, null: false
       t.date :due_date, null: false
       t.decimal :total_amount, null: false, precision: 10, scale: 2
