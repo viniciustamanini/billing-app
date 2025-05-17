0ff62bef6b2c01189d3cf5c8824b92095d29348a
Vinicius <vinitamanini@hotmail.com>
2025-05-16 23:43:30 -0300

    feat: link user by cpf when creating a new profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -9,11 +9,22 @@ class Profile < ApplicationRecord
   scope :default, -> { where(default_profile: true) }
   scope :by_type, ->(type_name) { joins(:profile_type).where(profile_types: { name: type_name }) }
 
+  after_create :link_user_by_cpf
+
   def full_name
     if user.present?
       "#{user.first_name} #{user.last_name}"
     else
       "#{first_name} #{last_name}"
     end
+  end
+
+  private
+
+  def link_user_by_cpf
+    return if user.present? || cpf.blank?
+
+    matching_user = User.find_by(cpf: cpf)
+    update_column(:user_id, matching_user.id) if matching_user
   end
 end
