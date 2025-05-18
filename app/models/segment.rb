4aafa357ebb270d2166850832192452ace6e93db
Vinicius Tamanini <vinitamanini@hotmail.com>
2025-05-18 10:17:54 -0400

    feat: checks if debt max is greater than debt min on segment

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -1,4 +1,13 @@
 class Segment < ApplicationRecord
   belongs_to :company
   belongs_to :overdue_range
+
+  validate :debt_max_greater_than_debt_min
+
+  private
+
+  def debt_max_greater_than_debt_min
+    return if debt_min.blank? || debt_max.blank?
+    errors.add(:debt_max, "must be greater than debt min") if debt_min > debt_max
+  end
 end
