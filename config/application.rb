8d99513138130d0c68b1ee81988d445d2bbcff56
Vinicius <vinitamanini@hotmail.com>
2025-05-22 20:56:51 -0300

    change default locale

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,6 @@ module BillingApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.available_locales = [ :en, :pt ]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :pt
   end
 end
