61c109823590048e96d7ab94e6f2fce7b701f5f0
Vinicius <vinitamanini@hotmail.com>
2025-05-24 14:24:30 -0300

    feat: customer invoices helper status badge

--- app/helpers/customer_dashboard_helper.rb
+++ app/helpers/customer_dashboard_helper.rb
@@ -1,2 +1,13 @@
 module CustomerDashboardHelper
+  def due_date_status_badge(due_date)
+    today = Date.current
+
+    if due_date > today
+      { title: "A VENCER", class: "bg-green-100 text-green-800" }
+    elsif due_date == today
+      { title: "VENCE HOJE", class: "bg-yellow-100 text-yellow-800" }
+    else
+      { title: "VENCIDA", class: "bg-red-100 text-red-800" }
+    end
+  end
 end
